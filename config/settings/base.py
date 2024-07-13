e1a87d18b967bd0a480a00ccea91032c0a65d185
nickmoreton <nick.moreton@torchbox.com>
2024-07-13 22:39:55 +0100

    Fix static locations

--- config/settings/base.py
+++ config/settings/base.py
@@ -117,7 +117,7 @@ STATICFILES_FINDERS = [
 ]
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "core/static"),
+    os.path.join(BASE_DIR, "core/static_src"),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
